24f4d443a8412a2a0673b314e9a1b4f4c47147cc
lumilitas <lucas23121995>
2020-08-27 10:54:17 -0300

    Implemented head fragment

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -1,8 +1,8 @@
 package com.example.android.android_me.ui;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
