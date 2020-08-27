b037f83682525c26187d6a2f2b875298e9ea7c98
lumilitas <lucas23121995>
2020-08-27 10:33:52 -0300

    first commit

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,37 @@
+package com.example.android.android_me.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+public class BodyPartFragment extends Fragment {
+    // Mandatory constructor for instatiating the fragment
+    public BodyPartFragment() {
+    }
+
+    /*
+    * Inflates the fragment layout and sets any image resources
+    * */
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the Android-Me fragment layout
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+
+        //Get a reference to the ImageView in the fragment layout
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.bodyPartImageView);
+
+        // Set the image resource to display
+        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+
+        // Return root view
+        return rootView;
+    }
+}
